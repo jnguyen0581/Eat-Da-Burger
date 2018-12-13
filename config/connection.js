532e06eed8447c644c61fdc023d0d0bfc5fa1408
JoyceNguyen <jnguyen0581@gmail.com>
2018-12-12 23:42:03 -0600

    updated files

---
+++ config/connection.js
@@ -0,0 +1,30 @@
+var mysql = require("mysql");
+
+var connection;
+
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+
+
+connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Darcy0518*",
+  database: "burgers_db"
+
+});
+
+};
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+module.exports = connection;

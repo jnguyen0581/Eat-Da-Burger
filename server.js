934c053aba9a3c67085d9c34dbb9840854c7a52f
JoyceNguyen <jnguyen0581@gmail.com>
2019-01-02 21:29:33 -0600

    changes in database

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var methodOverride = require('method-override')
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-var db = process.env.DATABASE_URL || 'localhost'
+var db = process.env.DATABASE_URL || 'localhost';
 
 app.use(express.static(process.cwd() + '/public'));
 app.use(bodyParser.urlencoded({
 	extended: false
-}))
+}));
 // override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
